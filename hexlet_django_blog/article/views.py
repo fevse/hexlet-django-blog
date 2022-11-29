410e1daffd4e117db662734ccbd64d67e1990e92
fevse <fedulov.v.s@gmail.com>
2022-11-29 20:21:03 +0600

    update hexlet_django_blog

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('article')
+    return render(request, 'article/index.html', context={
+        'title': "Статьи"
+    })
