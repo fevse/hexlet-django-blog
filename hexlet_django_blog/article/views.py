65edfda3ee6af39cdf80e080737b92e2d1dabad9
fevse <fedulov.v.s@gmail.com>
2022-12-07 14:54:14 +0600

    update article

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Article
 
 
 
 class IndexView(View):
-
         
     def get(self, request, tags=None, article_id=None):
+        articles = Article.objects.all()
         return render(request, 'article/index.html', context={
-            'title': "Статьи", 'tags': tags, 'article_id': article_id,
+            'articles': articles,
+            'title': "Статьи", 
+            'tags': tags, 
+            'article_id': article_id,
         })
         
