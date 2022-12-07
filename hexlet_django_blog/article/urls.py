d8ea8b066dfd78aaf1d3d0c38c976ef17e75455a
fevse <fedulov.v.s@gmail.com>
2022-12-07 23:51:20 +0600

    update article

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from hexlet_django_blog.article import views
-from hexlet_django_blog.article.views import IndexView
+from hexlet_django_blog.article.views import IndexView, ArticleView
 
 
 urlpatterns = [
-    path('', IndexView.as_view()),
-    path('<str:tags>/<int:article_id>/', IndexView.as_view(), name='article')
+    path('', IndexView.as_view(), name='articles_index'),
+    path('<int:id>/', ArticleView.as_view(), name='article_show')
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from .models import Article
 
@@ -6,7 +6,16 @@ from .models import Article
 class IndexView(View):
         
     def get(self, request, *args, **kwargs):
-        articles = Article.objects.all()[:5]
+        articles = Article.objects.all()[:3]
         return render(request, 'article/index.html', context={
             'articles': articles,
+        })
+
+
+class ArticleView(View):
+
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, id=kwargs['id'])
+        return render(request, 'article/show.html', context={
+            'article': article,
         })
