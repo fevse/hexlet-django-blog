547ec792f525294136312725b98a83f74b3929df
fevse <fedulov.v.s@gmail.com>
2022-12-08 20:39:03 +0600

    add search function

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 from .models import Article
+from django.db.models import Q
+
 
 
 class IndexView(View):
         
     def get(self, request, *args, **kwargs):
-        articles = Article.objects.all()[:3]
+        query = request.GET.get('q', None)
+        if query:
+            articles = Article.objects.filter(Q(name__icontains=query))
+        else:
+            articles = Article.objects.all()[:5]
         return render(request, 'article/index.html', context={
             'articles': articles,
         })
