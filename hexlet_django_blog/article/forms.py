cdab0fc62ba655982b91b6171233c09effcb331d
fevse <fedulov.v.s@gmail.com>
2022-12-09 23:54:06 +0600

    update article.create

--- hexlet_django_blog/article/forms.py
+++ hexlet_django_blog/article/forms.py
@@ -1,7 +1,10 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, CharField
 from .models import Article
 
 class ArticleForm(ModelForm):
+
+    name = CharField(max_length=49, required=True)
+
     class Meta:
         model = Article
         fields = ['name', 'body']
